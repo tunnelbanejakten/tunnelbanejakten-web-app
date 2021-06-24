4bf8d9331f2f85b2f029abfc5243c301c27654f3
Mikael Svensson <mikael.svensson@klarna.com>
2021-06-24 20:10:51 +0200

    API connectivity check #3

--- src/views/device-test/Connectivity.vue
+++ src/views/device-test/Connectivity.vue
@@ -1,22 +1,69 @@
 <template>
   <div>
-    <p>Ett par automatiska test:</p>
-    <p>Uppkoppling till Tunnelbanejaktens server</p>
-    <p>Spara kaka med anonymt</p>
+    <p>För att allt ska fungera måste din mobil få kontakt med vår server.</p>
+    <Button
+      v-if="!isChecking"
+      label="Testa uppkopplingen"
+      @click="onStartTest"
+    />
+    <p v-if="isCheckInitiated">{{ backendStatus }}</p>
   </div>
 </template>
 
 <script lang="ts">
-import store, { Status } from '@/store'
-import { Component, Vue, Emit } from 'vue-property-decorator'
+import store, { Status } from "@/store";
+import { Component, Vue, Watch } from "vue-property-decorator";
+import Button from '@/components/common/Button.vue'
+
+const BackendStatus = {
+  UNKNOWN: "Vi vet inte om vår server är vaken eller inte.",
+  CHECKING: "Vi kollar om du har kontakt med vår server.",
+  ONLINE: "Vi har kontakt med vår server.",
+  INVALID_RESPONSE: "Något är fel med vår server.",
+  FAILED: "Det gick inte att få kontakt med vår server.",
+};
+
+const apiHost = process.env.VUE_APP_API_HOST;
 
 @Component({
   components: {
-  }
+    Button,
+  },
 })
 export default class Connectity extends Vue {
-  mounted () {
-    store.setDeviceTestStatus('connectivity', Status.SUCCESS)
+  private backendStatus = BackendStatus.UNKNOWN;
+  
+  get isChecking() {
+    return this.backendStatus === BackendStatus.CHECKING;
+  }
+  
+  get isCheckInitiated() {
+    return this.backendStatus !== BackendStatus.UNKNOWN;
+  }
+  
+  async onStartTest() {
+    this.backendStatus = BackendStatus.CHECKING;
+    try {
+      const resp = await fetch(`${apiHost}/wp-json/tuja/v1/ping`);
+      const payload = await resp.json();
+      this.backendStatus = resp.ok ? BackendStatus.ONLINE : BackendStatus.INVALID_RESPONSE;
+    } catch (e) {
+      this.backendStatus = BackendStatus.FAILED;
+    }
+  }
+
+  @Watch("backendStatus")
+  onStatusChange(backendStatus: string) {
+    store.setDeviceTestStatus(
+      "connectivity",
+      {
+        [BackendStatus.UNKNOWN]: Status.PENDING,
+        [BackendStatus.CHECKING]: Status.PENDING,
+        [BackendStatus.ONLINE]: Status.SUCCESS,
+        [BackendStatus.FAILED]: Status.FAILURE,
+        [BackendStatus.INVALID_RESPONSE]: Status.FAILURE,
+      }[backendStatus] || Status.PENDING
+    );
   }
 }
 </script>
