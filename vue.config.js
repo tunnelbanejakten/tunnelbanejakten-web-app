7e9d6acc8cc6653a98a02e84cd3836f1aa664ed0
Mikael Svensson <mikael.svensson@klarna.com>
2021-07-09 23:07:50 +0200

    Favicons #4

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
 process.env.VUE_APP_VERSION = require('./package.json').version
 
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  pwa: {
+    name: 'Lagportal för Tunnelbanejakten',
+    themeColor: '#fdc648',
+    msTileColor: '#fdc648',
+    iconPaths: {
+      favicon32: 'manifest/favicon-32x32.png',
+      favicon16: 'manifest/favicon-16x16.png',
+      appleTouchIcon: 'manifest/apple-touch-icon.png',
+      maskIcon: 'manifest/safari-pinned-tab.svg',
+      msTileImage: 'manifest/mstil-150x150.png'
+    }    
+  }
 }
