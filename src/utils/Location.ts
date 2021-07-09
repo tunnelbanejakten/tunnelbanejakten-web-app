33c7e0117d8376a75fdaaa38d228f0a85b761c62
Mikael Svensson <mikael.svensson@klarna.com>
2021-07-09 17:25:05 +0200

    Fix accuracy threshold #17

--- src/utils/Location.ts
+++ src/utils/Location.ts
@@ -1,3 +1,3 @@
-const HIGH_ACCURACY_THRESHOLD = 150;
+const HIGH_ACCURACY_THRESHOLD = 50;
 
 export const isAccuratePosition = (meterAccuracy: number) => meterAccuracy > 0 && meterAccuracy <= HIGH_ACCURACY_THRESHOLD
