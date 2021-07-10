c8a99d0af7610e02a8b98fae09d738547d4ef4c5
Mikael Svensson <mikael.svensson@klarna.com>
2021-07-10 21:40:04 +0200

    Update version in package.json as part of deployment script

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,19 @@
 # abort on errors
 set -e
 
+# prepare build by updating version in package.json and by adding a git tag
+yarn version --no-git-tag-version
+git add package.json
+NEW_VERSION=$(cat package.json | jq -r '.version')
+git commit -m "Release ${NEW_VERSION}"
+git tag -a "v${NEW_VERSION}" -m "Release ${NEW_VERSION}"
+
+# compile and build app
 yarn run build
 
 cd dist
 
+# add host file used by github pages
 echo 'app.tunnelbanejakten.se' > CNAME
 
 git init
