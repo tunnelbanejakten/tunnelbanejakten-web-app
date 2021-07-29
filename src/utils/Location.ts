6f462e77ab8f08212dcf58acbcc3bf833df7a517
Mikael Svensson <mikael.svensson@klarna.com>
2021-07-29 12:23:38 +0200

    Read high-accuracy threshold limit from config file #26

--- src/utils/Location.ts
+++ src/utils/Location.ts
@@ -1,3 +1,3 @@
-const HIGH_ACCURACY_THRESHOLD = 50
+const HIGH_ACCURACY_THRESHOLD = parseInt(process.env.VUE_APP_HIGH_ACCURACY_THRESHOLD || '50', 10)
 
 export const isAccuratePosition = (meterAccuracy: number) => meterAccuracy > 0 && meterAccuracy <= HIGH_ACCURACY_THRESHOLD
