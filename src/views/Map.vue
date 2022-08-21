e42b0ab38d7031bfb15158bda070db5183bd26cf
Mikael Svensson <mikael.svensson@klarna.com>
2022-08-21 09:37:10 +0200

    Show map even if location is not available #118

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,49 +1,73 @@
 <template>
   <Page
     title="Karta"
-    :no-padding="!isError"
+    :no-padding="true"
   >
-    <div
-      class="no-map"
-      v-if="isLoadingMarkers"
-    >
-      <Loader :message="stateMessage" />
-    </div>
-    <div
-      class="no-map"
-      v-if="isError"
-    >
-      <Message
-        header="Problem med kartan"
-        headerIcon="map-marker-alt"
-        :message="stateMessage"
-        :type="stateMessageType"
-      >
-        <Button
-          label="Testa igen"
-          :wide="false"
-          @click="initLocationListener"
-        />
-      </Message>
-    </div>
-    <div
-      class="map-container"
-      v-if="!isLoadingMarkers && !isError"
-    >
+    <div class="map-container">
       <MapComponent
         :markers="mapObjects"
         @marker-clicked="onSelectCheckpoint"
       />
-      <ConfirmationOverlay
-        v-if="activeMarkers.length"
-        :question="atLocationText"
-        @accept="onReopenCheckpoint"
-        accept-label="Visa"
-      />
-      <NotificationOverlay
-        v-if="!!notification"
-        :message="notification"
-      />
+      <div class="overlay-container">
+        <div
+          v-if="activeMarkers.length"
+          class="task-status"
+        >
+          <div class="task-message">
+            <p>{{ atLocationText }}</p>
+            <div>
+              <Button
+                label="Visa"
+                @click="onReopenCheckpoint"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div
+          v-if="isLoadingMarkers"
+          class="task-status"
+        >
+          <div class="task-icon">
+            <Loader size="small" />
+          </div>
+          <div class="task-message">
+            {{ markersStateMessage }}
+          </div>
+        </div>
+        <div v-if="isPositioningFailed">
+          <Message
+            header="Problem med kartan"
+            headerIcon="map-marker-alt"
+            :message="positioningStateMessage"
+            type="failure"
+          >
+            <Button
+              label="Testa igen"
+              :wide="false"
+              @click="initLocationListener"
+            />
+          </Message>
+        </div>
+        <div
+          class="task-status"
+          v-if="!isPositioningFailed"
+        >
+          <div class="task-icon">
+            <AccuracyIcon
+              :accuracy="accuracyLevel"
+              v-if="isPositionAcquired"
+            />
+            <Loader
+              v-if="!isPositionAcquired"
+              size="small"
+            />
+          </div>
+          <div class="task-message">
+            {{ positioningStateMessage }}
+          </div>
+        </div>
+      </div>
       <Fullscreen
         v-if="isCheckpointSelectorShown"
         @close="onCloseCheckpointSelector"
@@ -109,6 +133,7 @@ import MapComponent, {
 } from '@/components/common/Map.vue'
 import ConfirmationOverlay from '@/components/common/ConfirmationOverlay.vue'
 import NotificationOverlay from '@/components/common/NotificationOverlay.vue'
+import AccuracyIcon from '@/components/common/AccuracyIcon.vue'
 import Button from '@/components/common/Button.vue'
 import Fullscreen from '@/components/common/Fullscreen.vue'
 import Loader from '@/components/common/Loader.vue'
@@ -125,13 +150,18 @@ import store from '@/store'
 
 const apiHost = process.env.VUE_APP_API_HOST
 
-enum State {
-  INITIAL,
-  LOADING_MARKERS,
-  MARKERS_LOADED,
-  LOADING_POSITION,
-  POSITION_ACQUIRED,
-  ERROR
+enum MarkersState {
+  NOT_REQUESTED,
+  LOADING,
+  LOADED,
+  FAILED
+}
+
+enum PositioningState {
+  NOT_REQUESTED,
+  LOADING,
+  ACQUIRED,
+  FAILED
 }
 
 enum CheckpointView {
@@ -215,14 +245,16 @@ const removeUnavailableStationsFilter = (m: Marker): boolean => {
     CheckpointQuestion,
     CheckpointStation,
     CheckpointSelector,
-    Loader
+    Loader,
+    AccuracyIcon
   }
 })
 export default class Map extends Vue {
-  private state: State = State.INITIAL;
-  private notification = '';
-  private stateMessage = '';
-  private stateMessageType: MessageType = MessageType.FAILURE;
+  private markersState: MarkersState = MarkersState.NOT_REQUESTED;
+  private markersStateMessage: string = '';
+  private positioningState: PositioningState = PositioningState.NOT_REQUESTED;
+  private positioningStateMessage: string = 'Startar GPS';
+
   private watchId = 0;
   private checkpointView: CheckpointView = CheckpointView.NONE
   private lastApproxAccuracy = -1;
@@ -243,14 +275,33 @@ export default class Map extends Vue {
   private selectedCheckpoint: CheckpointMarker | null = null;
   private selectedTicket?: TicketData;
 
-  updateState(newState: State, newStateMessage: string) {
-    this.state = newState
-    this.stateMessage = newStateMessage
+  updateMarkersState(newState: MarkersState, newStateMessage: string) {
+    this.markersState = newState
+    this.markersStateMessage = newStateMessage
 
-    Analytics.logEvent(Analytics.AnalyticsEventType.MAP, 'set', 'state', {
-      state: State[this.state],
-      message: this.stateMessage
+    Analytics.logEvent(Analytics.AnalyticsEventType.MAP, 'set', 'markers_state', {
+      state: MarkersState[this.markersState],
+      message: this.markersStateMessage
     })
+  }
+
+  updatePositioningState(newState: PositioningState, newStateMessage: string) {
+    this.positioningState = newState
+    this.positioningStateMessage = newStateMessage
+
+    Analytics.logEvent(Analytics.AnalyticsEventType.MAP, 'set', 'positioning_state', {
+      state: PositioningState[this.positioningState],
+      message: this.positioningStateMessage
+    })
+  }
+
+  toAccuracyLevel(value: string) {
+    return {
+      'HIGHEST': LocationUtils.AccuracyLevel.HIGHEST,
+      'HIGH': LocationUtils.AccuracyLevel.HIGH,
+      'MEDIUM': LocationUtils.AccuracyLevel.MEDIUM,
+      'LOW': LocationUtils.AccuracyLevel.LOW,
+    }[value]
   }
 
   get isCheckpointSelectorShown() {
@@ -273,14 +324,10 @@ export default class Map extends Vue {
     return this.userPositions.length ? { ...this.userPositions[this.userPositions.length - 1] } : null
   }
 
-  get isError() {
-    return this.state === State.ERROR
-  }
-
   get isLoadingMarkers() {
     return (
-      this.state === State.INITIAL ||
-      this.state === State.LOADING_MARKERS
+      this.markersState === MarkersState.NOT_REQUESTED ||
+      this.markersState === MarkersState.LOADING
     )
   }
 
@@ -385,14 +432,9 @@ export default class Map extends Vue {
   updateActiveMarkers(markers: Marker[], position: Marker) {
     const isPositionAccurate = this.isAccurateEnough(position.meterAccuracy)
     if (!isPositionAccurate) {
-      this.notification = this.isLowAccuracyAllowed
-        ? 'Din GPS är inte tillräckligt exakt just nu. Kontakta kundtjänst för att få hjälp.'
-        : 'Vi är osäkra på din position. Stå still ett litet tag så löser det sig säkert.'
       this.activeMarkers = []
       return
     }
-    this.notification = ''
-
     const isMarkerActiveBefore = this.activeMarkers.length > 0
     this.activeMarkers = markers
       .filter((marker: Marker) => !(marker instanceof StartPositionMarker))
@@ -461,8 +503,20 @@ export default class Map extends Vue {
     return this.markers.length > 0
   }
 
+  get isPositionAcquired(): boolean {
+    return this.positioningState === PositioningState.ACQUIRED
+  }
+
+  get isPositioningFailed(): boolean {
+    return this.positioningState === PositioningState.FAILED
+  }
+
+  get accuracyLevel(): LocationUtils.AccuracyLevel {
+    return LocationUtils.getAccuracyLevel(this.currentPosition.meterAccuracy)
+  }
+
   async loadMarkers() {
-    this.updateState(State.LOADING_MARKERS, 'Hämtar karta')
+    this.updateMarkersState(MarkersState.LOADING, 'Hämtar karta')
     try {
       const resp = await Api.call({
         endpoint: `${apiHost}/wp-json/tuja/v1/map/markers`
@@ -521,27 +575,25 @@ export default class Map extends Vue {
               count: this.markers.length
             }
           )
-          this.updateState(State.MARKERS_LOADED, 'Vi har hämtat kartmarkörerna.')
+          this.updateMarkersState(MarkersState.LOADED, 'Vi har hämtat kartmarkörerna.')
           return true
         } else {
-          this.updateState(
-            State.ERROR,
+          this.updateMarkersState(MarkersState.FAILED,
             'Det finns inga kontroller att visa på kartan.'
           )
         }
       } else if (resp.status === 204) {
-        this.updateState(
-          State.ERROR,
+        this.updateMarkersState(MarkersState.FAILED,
           'Ert lag har inte blivit tilldelad en karta. Kontakta kundtjänst så löser de detta.'
         )
       } else {
-        this.updateState(State.ERROR, 'Kunde inte läsa in kontroller.')
+        this.updateMarkersState(MarkersState.FAILED, 'Kunde inte läsa in kontroller.')
       }
     } catch (e: any) {
       if (e instanceof Api.NotSignedInError) {
-        this.updateState(State.ERROR, 'Du är inte inloggad.')
+        this.updateMarkersState(MarkersState.FAILED, 'Du är inte inloggad.')
       } else {
-        this.updateState(State.ERROR, 'Kunde inte läsa in kontroller.')
+        this.updateMarkersState(MarkersState.FAILED, 'Kunde inte läsa in kontroller.')
       }
     }
     return false
@@ -564,19 +616,33 @@ export default class Map extends Vue {
     this.lastApproxAccuracy = approxAccuracy
   }
 
+  updateUserPosition({ latitude, longitude }: Coord, meterAccuracy: number) {
+    const newCurrentPosition = new UserPositionMarker()
+    newCurrentPosition.meterAccuracy = meterAccuracy
+    newCurrentPosition.latitude = latitude
+    newCurrentPosition.longitude = longitude
+    newCurrentPosition.timestamp = Date.now()
+    newCurrentPosition.showAccuracyCircle = store.state.debugSettings.map
+
+    this.currentPosition = newCurrentPosition
+
+    this.userPositions.push(newCurrentPosition)
+    const userPositionsHistoryLimit = store.state.debugSettings.map ? MAX_BREADCRUMB_COUNT : 1
+    if (this.userPositions.length > userPositionsHistoryLimit) {
+      this.userPositions.splice(0, this.userPositions.length - userPositionsHistoryLimit)
+    }
+  }
+
   initLocationListener() {
     if ('geolocation' in navigator) {
-      this.updateState(
-        State.LOADING_POSITION,
+      this.updatePositioningState(
+        PositioningState.LOADING,
         'Försöker hittar dig på kartan.'
       )
       if (this.watchId) {
         navigator.geolocation.clearWatch(this.watchId)
       }
-      if (this.lowAccuracyTimeoutId) {
-        clearTimeout(this.lowAccuracyTimeoutId)
-        this.lowAccuracyTimeoutId = 0
-      }
+      this.stopLowAccuracyTimer()
       this.watchId = navigator.geolocation.watchPosition(
         position => {
           const {
@@ -598,27 +664,11 @@ export default class Map extends Vue {
             }
           }
 
-          const newCurrentPosition = new UserPositionMarker()
-          newCurrentPosition.meterAccuracy = accuracy
-          newCurrentPosition.latitude = latitude
-          newCurrentPosition.longitude = longitude
-          newCurrentPosition.timestamp = Date.now()
-          newCurrentPosition.showAccuracyCircle = store.state.debugSettings.map
+          this.updateUserPosition({ latitude, longitude }, accuracy)
 
-          this.currentPosition = newCurrentPosition
-
-          this.userPositions.push(newCurrentPosition)
-          const userPositionsHistoryLimit = store.state.debugSettings.map ? MAX_BREADCRUMB_COUNT : 1
-          if (this.userPositions.length > userPositionsHistoryLimit) {
-            this.userPositions.splice(0, this.userPositions.length - userPositionsHistoryLimit)
-          }
-
-          if (this.state !== State.POSITION_ACQUIRED) {
-            this.updateState(
-              State.POSITION_ACQUIRED,
-              'Vi har hittat dig på kartan.'
-            )
-            if (!this.lowAccuracyTimeoutId) {
+          const isPositionAccurate = this.isAccurateEnough(accuracy)
+          if (this.positioningState !== PositioningState.ACQUIRED) {
+            if (!this.lowAccuracyTimeoutId && !isPositionAccurate) {
               const configuredTimeout = store.state.configuration.positioning.highAccuracyTimeout
               const defaultTimeout = parseInt(process.env.VUE_APP_HIGH_ACCURACY_TIMEOUT || '60', 10)
               const accuracyTimeout = configuredTimeout || defaultTimeout
@@ -641,34 +691,48 @@ export default class Map extends Vue {
             }
           }
 
+          if (isPositionAccurate && this.lowAccuracyTimeoutId) {
+            this.stopLowAccuracyTimer()
+          }
+
+          const positioningStatusMessage = isPositionAccurate
+            ? 'Vi har hittat dig på kartan.'
+            : (this.isLowAccuracyAllowed
+              ? 'Din GPS är inte tillräckligt exakt just nu. Kontakta kundtjänst för att få hjälp.'
+              : 'Vi är osäkra på din position. Stå still ett litet tag så löser det sig säkert.')
+          this.updatePositioningState(
+            PositioningState.ACQUIRED,
+            positioningStatusMessage
+          )
+
           this.logAccuracy(accuracy)
         },
         error => {
           switch (error.code) {
             // 1 PERMISSION_DENIED The acquisition of the geolocation information failed because the page didn't have the permission to do it.
             case 1:
-              this.updateState(
-                State.ERROR,
-                'Antingen är din GPS inte påslagen eller så har du blockerat du den här sidan från att använda den.'
+              this.updatePositioningState(
+                PositioningState.FAILED,
+                'Är din GPS påslagen? Har du blockerat den här sidan från att använda den?'
               )
               break
             // 2 POSITION_UNAVAILABLE The acquisition of the geolocation failed because one or several internal sources of position returned an internal error.
             case 2:
-              this.updateState(
-                State.ERROR,
-                'Det gick inte att fixera din position. Kanske åker du bil eller är på en plats med dålig mottagning?'
+              this.updatePositioningState(
+                PositioningState.FAILED,
+                'Av någon anledning kunde vi inte ta reda på din position.'
               )
               break
             // 3 TIMEOUT The time allowed to acquire the geolocation, defined by PositionOptions.timeout information that was reached before the information was obtained.
             case 3:
-              this.updateState(
-                State.ERROR,
+              this.updatePositioningState(
+                PositioningState.FAILED,
                 'Det tog för lång tid att ta reda på din position så vi gav upp.'
               )
               break
             default:
-              this.updateState(
-                State.ERROR,
+              this.updatePositioningState(
+                PositioningState.FAILED,
                 'Av någon anledning kunde vi inte ta reda på din position.'
               )
               break
@@ -681,7 +745,7 @@ export default class Map extends Vue {
         }
       )
     } else {
-      this.updateState(State.ERROR, 'Du saknar GPS.')
+      this.updatePositioningState(PositioningState.FAILED, 'Du saknar GPS.')
     }
   }
 
@@ -692,14 +756,18 @@ export default class Map extends Vue {
     }
   }
 
-  stopLocationListener() {
-    if (this.watchId) {
-      navigator.geolocation.clearWatch(this.watchId)
-    }
+  stopLowAccuracyTimer() {
     if (this.lowAccuracyTimeoutId) {
       clearTimeout(this.lowAccuracyTimeoutId)
       this.lowAccuracyTimeoutId = 0
     }
+  }
+
+  stopLocationListener() {
+    if (this.watchId) {
+      navigator.geolocation.clearWatch(this.watchId)
+    }
+    this.stopLowAccuracyTimer()
   }
 
   beforeDestroy() {
@@ -732,14 +800,6 @@ export default class Map extends Vue {
   margin: 10px;
 }
 
-.no-map {
-  display: flex;
-  height: 100%;
-  width: 100%;
-  justify-content: center;
-  align-items: center;
-}
-
 .arrival-container {
   display: flex;
   flex-direction: column;
@@ -747,5 +807,37 @@ export default class Map extends Vue {
   height: 100%;
   width: 100%;
   align-items: center;
+}
+
+.task-status {
+  background-color: rgba(255, 255, 255, 0.9);
+  display: flex;
+  flex-direction: row;
+  border-radius: 10px;
+  padding: 10px;
+  margin-top: 10px;
+}
+
+.task-icon {
+  padding: 0 10px 0 0;
+}
+
+.task-message p {
+  margin: 0 0 10px 0;
+}
+
+.overlay-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  z-index: 1000; /* To put it above the map */
+  /* text-align: center; */
+  position: absolute;
+  /* background-color: rgba(255, 255, 255, 0.9); */
+  bottom: 20px;
+  right: 20px;
+  left: 20px;
+  /* min-height: 20%; */
+  color: #000;
 }
 </style>
